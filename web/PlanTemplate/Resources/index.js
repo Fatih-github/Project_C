d091625f6d63d49c0ceb47677ddaca235c923bd5
Joy <0968294@hr.nl>
2020-11-27 11:10:43 +0100

    Er wordt nu een random seat gelogd wanneer er op de knop "Check for seats" wordt gedrukt.

---
+++ web/PlanTemplate/Resources/index.js
@@ -0,0 +1,17 @@
+
+
+
+    if (document.getElementById('confirm').clicked == true) {
+
+        let name = (document.getElementById("name").value);
+        let email = document.getElementById('email').value;
+
+
+        if (name != "" && email != "") {
+            console.log(name + email)
+        }
+    }
+
+
+
+
